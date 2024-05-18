99fa0c9721365636082ff622fd539bf82e6956f6
Vitaliy <dr.kovalyk2017@yandex.ru>
2024-05-19 01:29:38 +0300

    update touch

--- src/components/SwipeCard.js
+++ src/components/SwipeCard.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import TinderCard from "react-tinder-card";
 import "./SwipeCard.css";
 
 const SwipeCard = ({ character, onSwipe, onCardLeftScreen }) => {
   const [item, setItem] = useState(0);
+  const touchStartX = useRef(null);
+  const touchEndX = useRef(null);
+
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.targetTouches[0].clientX;
+  };
+
+  const handleTouchMove = (e) => {
+    touchEndX.current = e.targetTouches[0].clientX; 
+  };
+
+  const handleTouchEnd = () => {
+    const threshold = 50;
+    if(touchStartX.current !== null && touchEndX.current !== null) {
+      const distance = touchStartX - touchEndX.current;
+      if (Math.abs(distance) < threshold) {
+        newItem();
+      }
+    }
+    touchEndX.current = null;
+    touchStartX.current = null;
+  };
 
   const newItem = () => {
     setItem((prevItem) => {
@@ -21,7 +43,9 @@ const SwipeCard = ({ character, onSwipe, onCardLeftScreen }) => {
       <div
         style={{ backgroundImage: "url( " + character.url[item] + " )" }}
         className="card"
-        onClick={newItem}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
       >
         <h3>{character.name}</h3>
       </div>
