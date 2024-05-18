7787e3664f8887cd9be3a072637d3391efda06b3
Vitaliy <dr.kovalyk2017@yandex.ru>
2024-05-19 00:47:41 +0300

    creating app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import SwipeCard from './components/SwipeCard';
 import './App.css';
 
+const characters = [
+  {
+    name: 'Alanya',
+    url: ['https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg',
+    'https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg',
+    'https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg',
+    ]
+  },
+  {
+    name: 'Antalya',
+    url: ['https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg',
+    'https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg',
+    'https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg',
+    ]
+  },
+  {
+    name: 'Izmir',
+    url: ['https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg',
+    'https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg',
+    'https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg',
+    ]
+  },
+];
+
 function App() {
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="cardContainer">
+        {characters.map((character) => (
+          <SwipeCard
+            key={character.name}
+            character={character}
+            onSwipe={swiped}
+            onCardLeftScreen={outOfFrame}
+          />
+        ))}
+      </div>
     </div>
   );
 }
