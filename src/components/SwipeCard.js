e53c107aae32cb91e65b2a4673ba5ec788c3f5c5
Vitaliy <dr.kovalyk2017@yandex.ru>
2024-05-19 02:01:20 +0300

    update threshold

--- src/components/SwipeCard.js
+++ src/components/SwipeCard.js
@@ -1,37 +1,86 @@
-import React, { useState, useRef } from "react";
+// import React, { useState, useRef } from "react";
+// import { useGesture } from "@use-gesture/react";
+// import TinderCard from "react-tinder-card";
+// import "./SwipeCard.css";
+
+// const SwipeCard = ({ character, onSwipe, onCardLeftScreen }) => {
+//   const [item, setItem] = useState(0);
+//   const touchStartX = useRef(null);
+//   const touchEndX = useRef(null);
+
+//   const handleTouchStart = (e) => {
+//     touchStartX.current = e.targetTouches[0].clientX;
+//   };
+
+//   const handleTouchMove = (e) => {
+//     touchEndX.current = e.targetTouches[0].clientX; 
+//   };
+
+//   const handleTouchEnd = () => {
+//     const threshold = 2;
+//     if(touchStartX.current !== null && touchEndX.current !== null) {
+//       const distance = touchStartX.current - touchEndX.current;
+//       if (Math.abs(distance) < threshold) {
+//         newItem();
+//       }
+//     }
+//     touchEndX.current = null;
+//     touchStartX.current = null;
+//   };
+
+//   const newItem = () => {
+//     setItem((prevItem) => {
+//       return prevItem === character.url.length - 1 ? 0 : prevItem + 1;
+//     });
+//   };
+
+//   return (
+//     <TinderCard
+//       className="swipe"
+//       onSwipe={(dir) => onSwipe(dir, character.name)}
+//       onCardLeftScreen={() => onCardLeftScreen(character.name)}
+//       preventSwipe={["up", "down"]}
+//     >
+//       <div
+//         style={{ backgroundImage: `url(${character.url[item]})` }}
+//         className="card"
+//         onTouchStart={handleTouchStart}
+//         onTouchMove={handleTouchMove}
+//         onTouchEnd={handleTouchEnd}
+//       >
+//         <h3>{character.name}</h3>
+//       </div>
+//     </TinderCard>
+//   );
+// };
+
+// export default SwipeCard;
+
+import React, { useState } from "react";
+import { useGesture } from "@use-gesture/react";
 import TinderCard from "react-tinder-card";
 import "./SwipeCard.css";
 
 const SwipeCard = ({ character, onSwipe, onCardLeftScreen }) => {
   const [item, setItem] = useState(0);
-  const touchStartX = useRef(null);
-  const touchEndX = useRef(null);
-
-  const handleTouchStart = (e) => {
-    touchStartX.current = e.targetTouches[0].clientX;
-  };
-
-  const handleTouchMove = (e) => {
-    touchEndX.current = e.targetTouches[0].clientX; 
-  };
-
-  const handleTouchEnd = () => {
-    const threshold = 2;
-    if(touchStartX.current !== null && touchEndX.current !== null) {
-      const distance = touchStartX.current - touchEndX.current;
-      if (Math.abs(distance) < threshold) {
-        newItem();
-      }
-    }
-    touchEndX.current = null;
-    touchStartX.current = null;
-  };
 
   const newItem = () => {
     setItem((prevItem) => {
       return prevItem === character.url.length - 1 ? 0 : prevItem + 1;
     });
   };
+
+  const bind = useGesture({
+    onDrag: ({ movement: [mx], memo }) => {
+      if (!memo) {
+        if (Math.abs(mx) < 10) {
+          newItem();
+          return [true];
+        }
+      }
+      return memo;
+    },
+  });
 
   return (
     <TinderCard
@@ -41,11 +90,9 @@ const SwipeCard = ({ character, onSwipe, onCardLeftScreen }) => {
       preventSwipe={["up", "down"]}
     >
       <div
+        {...bind()}
         style={{ backgroundImage: `url(${character.url[item]})` }}
         className="card"
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
       >
         <h3>{character.name}</h3>
       </div>
