42560327b1d7fa4a1db12bd173fe3adf33841720
Vitaliy <dr.kovalyk2017@yandex.ru>
2024-05-20 02:11:07 +0300

    create description and click for image

--- src/App.js
+++ src/App.js
@@ -1,28 +1,100 @@
-import React, { useState } from 'react';
-import SwipeCard from './components/SwipeCard';
-import './App.css';
+// import React, { useState } from "react";
+// import SwipeCard from "./components/SwipeCard";
+// import "./App.css";
+
+// const characters = [
+//   {
+//     name: "Alanya",
+//     url: [
+//       "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg",
+//       "https://static.prian.ru/uploads/2021_10/11/202110110212011781573346o.jpg",
+//       "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318924_28-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-29.jpg",
+//     ],
+//     description: "Описание апартаментов в Алании",
+//   },
+//   {
+//     name: "Antalya",
+//     url: [
+//       "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318934_61-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-63.jpg",
+//       "https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg",
+//       "https://pro-dachnikov.com/uploads/posts/2021-10/1633508122_15-p-krasivie-doma-v-turtsii-foto-18.jpg",
+//     ],
+//     description: "Описание апартаментов в Анталии",
+//   },
+//   {
+//     name: "Izmir",
+//     url: [
+//       "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318929_21-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-22.jpg",
+//       "https://pro-dachnikov.com/uploads/posts/2021-10/1633724253_48-p-doma-v-turtsii-foto-52.jpg",
+//       "https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg",
+//     ],
+//     description: "Описание апартаментов в Измире",
+//   },
+// ];
+
+// function App() {
+//   const [lastDirection, setLastDirection] = useState();
+
+//   const swiped = (direction, nameToDelete) => {
+//     setLastDirection(direction);
+//   };
+
+//   const outOfFrame = (name) => {
+//     console.log(name + " left the screen!");
+//   };
+
+//   return (
+//     <div className="app">
+//       <div className="cardContainer">
+//         {characters.map((character) => (
+//           <SwipeCard
+//             key={character.name}
+//             character={character}
+//             onSwipe={swiped}
+//             onCardLeftScreen={outOfFrame}
+//           />
+//         ))}
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// TEST
+
+import React, { useState } from "react";
+import SwipeCard from "./components/SwipeCard";
+import "./App.css";
 
 const characters = [
   {
-    name: 'Alanya',
-    url: ['https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg',
-    'https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg',
-    'https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg',
-    ]
+    name: "Alanya",
+    url: [
+      "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg",
+      "https://static.prian.ru/uploads/2021_10/11/202110110212011781573346o.jpg",
+      "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318924_28-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-29.jpg",
+    ],
+    description: "Описание апартаментов в Алании",
   },
   {
-    name: 'Antalya',
-    url: ['https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg',
-    'https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg',
-    'https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg',
-    ]
+    name: "Antalya",
+    url: [
+      "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318934_61-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-63.jpg",
+      "https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg",
+      "https://pro-dachnikov.com/uploads/posts/2021-10/1633508122_15-p-krasivie-doma-v-turtsii-foto-18.jpg",
+    ],
+    description: "Описание апартаментов в Анталии",
   },
   {
-    name: 'Izmir',
-    url: ['https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318944_4-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-4.jpg',
-    'https://respekt-an.ru/attachments/f/9/f96cc4c6bc1418efe8d77df9485f184e.jpg',
-    'https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg',
-    ]
+    name: "Izmir",
+    url: [
+      "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663318929_21-mykaleidoscope-ru-p-villi-v-turtsii-krasivo-22.jpg",
+      "https://pro-dachnikov.com/uploads/posts/2021-10/1633724253_48-p-doma-v-turtsii-foto-52.jpg",
+      "https://i.pinimg.com/originals/78/fb/a2/78fba2b7d5ddaef70f2d62c2435f4d3c.jpg",
+    ],
+    description: "Описание апартаментов в Измире",
   },
 ];
 
@@ -30,12 +102,11 @@ function App() {
   const [lastDirection, setLastDirection] = useState();
 
   const swiped = (direction, nameToDelete) => {
-    console.log('removing: ' + nameToDelete);
     setLastDirection(direction);
   };
 
   const outOfFrame = (name) => {
-    console.log(name + ' left the screen!');
+    console.log(name + " left the screen!");
   };
 
   return (
@@ -55,3 +126,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/SwipeCard.js
+++ src/components/SwipeCard.js
@@ -1,30 +1,112 @@
+// import React, { useState, useRef, useEffect } from "react";
+// import TinderCard from "react-tinder-card";
+// import "./SwipeCard.css";
+
+// const SwipeCard = ({
+//   character,
+//   onSwipe,
+//   onCardLeftScreen,
+// }) => {
+//   const [item, setItem] = useState(0);
+//   const touchStartX = useRef(null);
+//   const touchStartY = useRef(null);
+
+//   const handleTouchStart = (e) => {
+//     touchStartX.current = e.targetTouches[0].clientX;
+//     touchStartY.current = e.targetTouches[0].clientY;
+//   };
+
+//   const handleTouchEnd = (e) => {
+//     const touchEndX = e.changedTouches[0].clientX;
+//     const touchEndY = e.changedTouches[0].clientY;
+//     const threshold = 50;
+//     if (touchStartX.current !== null && touchEndX !== null) {
+//       const distanceX = touchStartX.current - touchEndX;
+//       const distanceY = touchStartY.current - touchEndY;
+
+//       if (Math.abs(distanceX) < threshold && Math.abs(distanceY) < threshold) {
+//         newItem();
+//       }
+//     }
+//     touchStartX.current = null;
+//     touchStartY.current = null;
+//   };
+
+//   const newItem = () => {
+//     setItem((prevItem) => {
+//       return prevItem === character.url.length - 1 ? 0 : prevItem + 1;
+//     });
+//   };
+
+//   return (
+//     <TinderCard
+//       className="swipe"
+//       onSwipe={(dir) => {
+//         onSwipe(dir, character.name);
+//       }}
+//       onCardLeftScreen={() => onCardLeftScreen(character.name)}
+//       preventSwipe={["up", "down"]}
+//     >
+//       <div
+//         style={{ backgroundImage: `url(${character.url[item]})` }}
+//         className="card"
+//         onTouchStart={handleTouchStart}
+//         onTouchEnd={handleTouchEnd}
+//       >
+//         <h3>{character.name}</h3>
+//         <div className="indicator-container">
+//           {character.url.map((_, index) => (
+//             <div
+//               key={index}
+//               className={`indicator ${index === item ? "active" : ""}`}
+//             ></div>
+//           ))}
+//         </div>
+//       </div>
+//     </TinderCard>
+//   );
+// };
+
+// export default SwipeCard;
+
+// TEST
+
 import React, { useState, useRef } from "react";
 import TinderCard from "react-tinder-card";
 import "./SwipeCard.css";
 
 const SwipeCard = ({ character, onSwipe, onCardLeftScreen }) => {
   const [item, setItem] = useState(0);
+  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
   const touchStartX = useRef(null);
-  // const touchEndX = useRef(null);
+  const touchStartY = useRef(null);
 
   const handleTouchStart = (e) => {
     touchStartX.current = e.targetTouches[0].clientX;
+    touchStartY.current = e.targetTouches[0].clientY;
   };
-
-  // const handleTouchMove = (e) => {
-  //   touchEndX.current = e.targetTouches[0].clientX; 
-  // };
 
   const handleTouchEnd = (e) => {
     const touchEndX = e.changedTouches[0].clientX;
-    const threshold = 50; // Порог для распознавания касания (можно настроить для чувствительности)
-    if (touchStartX.current !== null) {
-      const distance = touchStartX.current - touchEndX;
-      if (Math.abs(distance) < threshold) {
+    const touchEndY = e.changedTouches[0].clientY;
+    const threshold = 50;
+    if (touchStartX.current !== null && touchEndX !== null) {
+      const distanceX = touchStartX.current - touchEndX;
+      const distanceY = touchStartY.current - touchEndY;
+      console.log(distanceY);
+
+      if (distanceY > threshold) {
+        setIsDescriptionExpanded(true);
+      } else if (distanceY < threshold) {
+        setIsDescriptionExpanded(false);
+      }
+
+      if (Math.abs(distanceX) < threshold && Math.abs(distanceY) < threshold) {
         newItem();
       }
     }
     touchStartX.current = null;
+    touchStartY.current = null;
   };
 
   const newItem = () => {
@@ -34,24 +116,37 @@ const SwipeCard = ({ character, onSwipe, onCardLeftScreen }) => {
   };
 
   return (
-    <TinderCard
-      className="swipe"
-      onSwipe={(dir) => onSwipe(dir, character.name)}
-      onCardLeftScreen={() => onCardLeftScreen(character.name)}
-      preventSwipe={["up", "down"]}
-    >
-      <div
-        style={{ backgroundImage: `url(${character.url[item]})` }}
-        className="card"
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
+    <div onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
+      <TinderCard
+        className="swipe"
+        onSwipe={(dir) => {
+          onSwipe(dir, character.name);
+        }}
+        onCardLeftScreen={() => onCardLeftScreen(character.name)}
+        preventSwipe={["up", "down"]}
       >
-        <h3>{character.name}</h3>
+        <div
+          style={{ backgroundImage: `url(${character.url[item]})` }}
+          className="card"
+        >
+          <h3>{character.name}</h3>
+          <div className="indicator-container">
+            {character.url.map((_, index) => (
+              <div
+                key={index}
+                className={`indicator ${index === item ? "active" : ""}`}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </TinderCard>
+      <div
+        className={`description ${isDescriptionExpanded ? "expanded" : "none"}`}
+      >
+        <p className="description__text">{character.description}</p>
       </div>
-    </TinderCard>
+    </div>
   );
 };
 
 export default SwipeCard;
-
-
