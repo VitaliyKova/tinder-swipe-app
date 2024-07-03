a3181d42546905e64475827c6c09fb4327a1db34
Ilya <ok.brain@yandex.ru>
2024-07-03 14:17:24 +0300

    Стили

--- src/components/pages/AgentPage/index.jsx
+++ src/components/pages/AgentPage/index.jsx
@@ -106,7 +106,7 @@ const AgentPage = () => {
     };
 
     return (
-        <div className="full-info">
+        <div className="full-info agent-full-info">
             <div className="full-info__card">
                 <Header brocker={apartment}/>
                 <Filter aparts={globalObject.aparts} filterText={"Объектов"}/>
